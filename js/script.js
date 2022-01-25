f39cea94feed378fa0b1ac5ac7f3e80159e786a8
K.M. AL-AMIN <46156890+kmalaminstd@users.noreply.github.com>
2022-01-26 00:35:43 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//bx slider
+
+$(document).ready(function(){
+    $('.slider_area').bxSlider();
+    $('#main_menu').meanmenu({
+      meanMenuContainer: '#mobileMenu',
+      meanScreenWidth: 991,
+    });
+  });
